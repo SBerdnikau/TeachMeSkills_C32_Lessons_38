fbf6d41b60247676c7155d395e3ada03552fabeb
Sergey <berdnikausiarhei@gmail.com>
2025-03-05 08:19:44 +0300

    TMC-C32-HM=Lesson-38
    update aspect

--- src/main/java/com/teachmeskills/lesson_38/aspect/TimedAspect.java
+++ src/main/java/com/teachmeskills/lesson_38/aspect/TimedAspect.java
@@ -1,6 +1,5 @@
 package com.teachmeskills.lesson_38.aspect;
 
-import com.teachmeskills.lesson_38.annatation.Timed;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -14,16 +13,16 @@ public class TimedAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TimedAspect.class);
 
-    @Around("@annotation(timed)")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint, Timed timed) throws Throwable {
+    @Around("@annotation(com.teachmeskills.lesson_38.annatation.Timed)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
         long executionTime = System.currentTimeMillis() - startTime;
 
         if (executionTime > 1000) {
-            LOGGER.warn("Method {} completed in {} milliseconds.", joinPoint.getSignature(), executionTime);
+            LOGGER.warn("Method -> {} completed in [{}] milliseconds.", joinPoint.getSignature(), executionTime);
         }else {
-            LOGGER.info("Method {} completed in {} milliseconds.", joinPoint.getSignature(), executionTime);
+            LOGGER.info("Method -> {} completed in [{}] milliseconds.", joinPoint.getSignature(), executionTime);
         }
 
         return proceed;
